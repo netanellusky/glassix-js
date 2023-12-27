91ba90383db698d33c255add5c75a7e6ef737e0a
Netanel Lusky <netanell@consist-sys.consist.co.il>
2023-12-27 16:34:02 +0200

    Bug Fixes: Get PDF file with arraybuffer

--- src/API/tickets.js
+++ src/API/tickets.js
@@ -1,205 +1,264 @@
-import axios from 'axios';
-import catchError from '../helpers/catchError';
+import axios from "axios";
+import catchError from "../helpers/catchError";
 
 // TICKETS ENDPOINTS
 export const createTicket = async (ctx, payload = {}) => {
-  try {
-    const headers = await ctx.getRequestHeaders(ctx);
-    const res = await axios.post(`${ctx.url}/tickets/create`, payload, { headers });
-    return res?.data;
-  } catch (error) {
-    return catchError(error);
-  }
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.post(`${ctx.url}/tickets/create`, payload, {
+			headers,
+		});
+		return res?.data;
+	} catch (error) {
+		return catchError(error);
+	}
 };
 
 export const getTicket = async (ctx, ticketId) => {
-  try {
-    const headers = await ctx.getRequestHeaders(ctx);
-    const res = await axios.get(`${ctx.url}/tickets/get/${ticketId}`, { headers });
-    return res?.data;
-  } catch (error) {
-    return catchError(error);
-  }
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.get(`${ctx.url}/tickets/get/${ticketId}`, {
+			headers,
+		});
+		return res?.data;
+	} catch (error) {
+		return catchError(error);
+	}
 };
 
 export const getTicketsList = async (ctx, params = {}) => {
-  try {
-    const headers = await ctx.getRequestHeaders(ctx);
-    let tickets = [];
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		let tickets = [];
 
-    let count = 0;
-    let nextRequestUrl = null;
-    do
-    {
-      let res = null;
-      if (count === 0)
-      {
-        // first request
-        res = await axios.get(`${ctx.url}/tickets/list`, { headers, params });
-      }
-      else
-      {
-        // pagination
-        res = await axios.get(nextRequestUrl, { headers, params });
-      }
+		let count = 0;
+		let nextRequestUrl = null;
+		do {
+			let res = null;
+			if (count === 0) {
+				// first request
+				res = await axios.get(`${ctx.url}/tickets/list`, {
+					headers,
+					params,
+				});
+			} else {
+				// pagination
+				res = await axios.get(nextRequestUrl, { headers, params });
+			}
 
-      console.log(nextRequestUrl);
-      count += 1;
+			console.log(nextRequestUrl);
+			count += 1;
 
-      // Aggregate the response tickets
-      if (res?.data?.tickets && res?.data?.tickets.length)
-      {
-        console.warn(res?.data?.tickets.length);
-        tickets = tickets.concat(res?.data?.tickets);
-      }
+			// Aggregate the response tickets
+			if (res?.data?.tickets && res?.data?.tickets.length) {
+				console.warn(res?.data?.tickets.length);
+				tickets = tickets.concat(res?.data?.tickets);
+			}
 
-      // Let's check if we need to make more requests
-      nextRequestUrl = res?.data?.paging?.next;
-    }
-    while (nextRequestUrl && count < 15);
+			// Let's check if we need to make more requests
+			nextRequestUrl = res?.data?.paging?.next;
+		} while (nextRequestUrl && count < 15);
 
-    return tickets;
-  } catch (error) {
-    return catchError(error);
-  }
+		return tickets;
+	} catch (error) {
+		return catchError(error);
+	}
 };
 
 export const sendTicket = async (ctx, ticketId, payload = {}) => {
-  try {
-    const headers = await ctx.getRequestHeaders(ctx);
-    const res = await axios.post(`${ctx.url}/tickets/send/${ticketId}`, payload, { headers });
-    return res?.data;
-  } catch (error) {
-    return catchError(error);
-  }
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.post(
+			`${ctx.url}/tickets/send/${ticketId}`,
+			payload,
+			{ headers }
+		);
+		return res?.data;
+	} catch (error) {
+		return catchError(error);
+	}
 };
 
 export const setTicketState = async (ctx, ticketId, params = {}) => {
-  try {
-    const headers = await ctx.getRequestHeaders(ctx);
-    const res = await axios.put(`${ctx.url}/tickets/setstate/${ticketId}`, {}, { headers, params });
-    return res?.data;
-  } catch (error) {
-    return catchError(error);
-  }
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.put(
+			`${ctx.url}/tickets/setstate/${ticketId}`,
+			{},
+			{ headers, params }
+		);
+		return res?.data;
+	} catch (error) {
+		return catchError(error);
+	}
 };
 
 export const setTicketFields = async (ctx, ticketId, payload = {}) => {
-  try {
-    const headers = await ctx.getRequestHeaders(ctx);
-    const res = await axios.put(`${ctx.url}/tickets/setfields/${ticketId}`, payload, { headers });
-    return res?.data;
-  } catch (error) {
-    return catchError(error);
-  }
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.put(
+			`${ctx.url}/tickets/setfields/${ticketId}`,
+			payload,
+			{ headers }
+		);
+		return res?.data;
+	} catch (error) {
+		return catchError(error);
+	}
 };
 
 export const setTicketParticipantName = async (ctx, ticketId, payload = {}) => {
-  try {
-    const headers = await ctx.getRequestHeaders(ctx);
-    const res = await axios.put(`${ctx.url}/tickets/setparticipantname/${ticketId}`, payload, { headers });
-    return res?.data;
-  } catch (error) {
-    return catchError(error);
-  }
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.put(
+			`${ctx.url}/tickets/setparticipantname/${ticketId}`,
+			payload,
+			{ headers }
+		);
+		return res?.data;
+	} catch (error) {
+		return catchError(error);
+	}
 };
 
 export const setTicketOwner = async (ctx, ticketId, params = {}) => {
-  try {
-    const headers = await ctx.getRequestHeaders(ctx);
-    const res = await axios.put(`${ctx.url}/tickets/setowner/${ticketId}`, {}, { headers, params });
-    return res?.data;
-  } catch (error) {
-    return catchError(error);
-  }
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.put(
+			`${ctx.url}/tickets/setowner/${ticketId}`,
+			{},
+			{ headers, params }
+		);
+		return res?.data;
+	} catch (error) {
+		return catchError(error);
+	}
 };
 
 export const assignTicketAvailableUser = async (ctx, ticketId) => {
-  try {
-    const headers = await ctx.getRequestHeaders(ctx);
-    const res = await axios.put(`${ctx.url}/tickets/assignavailableuser/${ticketId}`, {}, { headers });
-    return res?.data;
-  } catch (error) {
-    return catchError(error);
-  }
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.put(
+			`${ctx.url}/tickets/assignavailableuser/${ticketId}`,
+			{},
+			{ headers }
+		);
+		return res?.data;
+	} catch (error) {
+		return catchError(error);
+	}
 };
 
 export const setTicketDepartment = async (ctx, ticketId, payload = {}) => {
-  try {
-    const headers = await ctx.getRequestHeaders(ctx);
-    const res = await axios.put(`${ctx.url}/tickets/setdepartment/${ticketId}`, payload, { headers });
-    return res?.data;
-  } catch (error) {
-    return catchError(error);
-  }
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.put(
+			`${ctx.url}/tickets/setdepartment/${ticketId}`,
+			payload,
+			{ headers }
+		);
+		return res?.data;
+	} catch (error) {
+		return catchError(error);
+	}
 };
 
 export const addTicketTags = async (ctx, ticketId, payload = {}) => {
-  try {
-    const headers = await ctx.getRequestHeaders(ctx);
-    const res = await axios.post(`${ctx.url}/tickets/addtags/${ticketId}`, payload, { headers });
-    return res?.data;
-  } catch (error) {
-    return catchError(error);
-  }
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.post(
+			`${ctx.url}/tickets/addtags/${ticketId}`,
+			payload,
+			{ headers }
+		);
+		return res?.data;
+	} catch (error) {
+		return catchError(error);
+	}
 };
 
 export const removeTicketTag = async (ctx, ticketId, params = {}) => {
-  try {
-    const headers = await ctx.getRequestHeaders(ctx);
-    const res = await axios.delete(`${ctx.url}/tickets/removetag/${ticketId}`, { headers, params });
-    return res?.data;
-  } catch (error) {
-    return catchError(error);
-  }
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.delete(
+			`${ctx.url}/tickets/removetag/${ticketId}`,
+			{ headers, params }
+		);
+		return res?.data;
+	} catch (error) {
+		return catchError(error);
+	}
 };
 
 export const addTicketNote = async (ctx, ticketId, payload = {}) => {
-  try {
-    const headers = await ctx.getRequestHeaders(ctx);
-    const res = await axios.post(`${ctx.url}/tickets/addnote/${ticketId}`, payload, { headers });
-    return res?.data;
-  } catch (error) {
-    return catchError(error);
-  }
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.post(
+			`${ctx.url}/tickets/addnote/${ticketId}`,
+			payload,
+			{ headers }
+		);
+		return res?.data;
+	} catch (error) {
+		return catchError(error);
+	}
 };
 
 export const scrambleTicket = async (ctx, ticketId) => {
-  try {
-    const headers = await ctx.getRequestHeaders(ctx);
-    const res = await axios.delete(`${ctx.url}/tickets/scramble/${ticketId}`, { headers });
-    return res?.data;
-  } catch (error) {
-    return catchError(error);
-  }
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.delete(
+			`${ctx.url}/tickets/scramble/${ticketId}`,
+			{ headers }
+		);
+		return res?.data;
+	} catch (error) {
+		return catchError(error);
+	}
 };
 
 export const pdfTicket = async (ctx, ticketId, payload = {}) => {
-  try {
-    const headers = await ctx.getRequestHeaders(ctx);
-    const res = await axios.post(`${ctx.url}/tickets/pdf/${ticketId}`, payload, { headers });
-    return res?.data;
-  } catch (error) {
-    return catchError(error);
-  }
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		headers["Accept"] = "application/pdf";
+		const res = await axios.post(
+			`${ctx.url}/tickets/pdf/${ticketId}`,
+			payload,
+			{
+				headers,
+				responseType: "arraybuffer",
+			}
+		);
+		return res?.data;
+	} catch (error) {
+		return catchError(error);
+	}
 };
 
 export const htmlTicket = async (ctx, ticketId, payload = {}) => {
-  try {
-    const headers = await ctx.getRequestHeaders(ctx);
-    const res = await axios.post(`${ctx.url}/tickets/html/${ticketId}`, payload, { headers });
-    return res?.data;
-  } catch (error) {
-    return catchError(error);
-  }
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.post(
+			`${ctx.url}/tickets/html/${ticketId}`,
+			payload,
+			{ headers }
+		);
+		return res?.data;
+	} catch (error) {
+		return catchError(error);
+	}
 };
 
 export const generateTicketSurveyLink = async (ctx, ticketId, payload = {}) => {
-  try {
-    const headers = await ctx.getRequestHeaders(ctx);
-    const res = await axios.post(`${ctx.url}/tickets/generatesurveylink/${ticketId}`, payload, { headers });
-    return res?.data;
-  } catch (error) {
-    return catchError(error);
-  }
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.post(
+			`${ctx.url}/tickets/generatesurveylink/${ticketId}`,
+			payload,
+			{ headers }
+		);
+		return res?.data;
+	} catch (error) {
+		return catchError(error);
+	}
 };
